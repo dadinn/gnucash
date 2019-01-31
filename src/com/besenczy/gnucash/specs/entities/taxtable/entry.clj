c95f483dd3d4ad3bc5093300a06e95bbc0f96d21
Daniel Dinnyes <ddinnyes@gmail.com>
2019-01-31 04:56:12 +0000

    moved taxtable-entry spec to its own sub-namespace to taxtable

--- src/com/besenczy/gnucash/specs/entities/taxtable/entry.clj
+++ src/com/besenczy/gnucash/specs/entities/taxtable/entry.clj
@@ -1,4 +1,4 @@
-(ns com.besenczy.gnucash.specs.entities.taxtable-entry
+(ns com.besenczy.gnucash.specs.entities.taxtable.entry
   (:require
    [com.besenczy.gnucash.specs.common :as common]
    [clojure.spec.alpha :as spec]))
