3de5fd6006ef197dea29fc463478317ff365f34e
Daniel Dinnyes <ddinnyes@gmail.com>
2019-03-05 09:31:08 +0000

    added xmlns prefixes to exported root element

--- src/com/besenczy/gnucash/export.clj
+++ src/com/besenczy/gnucash/export.clj
@@ -435,7 +435,39 @@
     (map budget-element budgets)
     (map countdata-element counters)))
 
+(def prefixes
+  {:xmlns/gnc "http://www.gnucash.org/XML/gnc"
+   :xmlns/act "http://www.gnucash.org/XML/act"
+   :xmlns/book "http://www.gnucash.org/XML/book"
+   :xmlns/cd "http://www.gnucash.org/XML/cd"
+   :xmlns/cmdty "http://www.gnucash.org/XML/cmdty"
+   :xmlns/price "http://www.gnucash.org/XML/price"
+   :xmlns/slot "http://www.gnucash.org/XML/slot"
+   :xmlns/split "http://www.gnucash.org/XML/split"
+   :xmlns/sx "http://www.gnucash.org/XML/sx"
+   :xmlns/trn "http://www.gnucash.org/XML/trn"
+   :xmlns/ts "http://www.gnucash.org/XML/ts"
+   :xmlns/fs "http://www.gnucash.org/XML/fs"
+   :xmlns/bgt "http://www.gnucash.org/XML/bgt"
+   :xmlns/recurrence "http://www.gnucash.org/XML/recurrence"
+   :xmlns/lot "http://www.gnucash.org/XML/lot"
+   :xmlns/addr "http://www.gnucash.org/XML/addr"
+   :xmlns/owner "http://www.gnucash.org/XML/owner"
+   :xmlns/billterm "http://www.gnucash.org/XML/billterm"
+   :xmlns/bt-days "http://www.gnucash.org/XML/bt-days"
+   :xmlns/bt-prox "http://www.gnucash.org/XML/bt-prox"
+   :xmlns/cust "http://www.gnucash.org/XML/cust"
+   :xmlns/employee "http://www.gnucash.org/XML/employee"
+   :xmlns/entry "http://www.gnucash.org/XML/entry"
+   :xmlns/invoice "http://www.gnucash.org/XML/invoice"
+   :xmlns/job "http://www.gnucash.org/XML/job"
+   :xmlns/order "http://www.gnucash.org/XML/order"
+   :xmlns/taxtable "http://www.gnucash.org/XML/taxtable"
+   :xmlns/tte "http://www.gnucash.org/XML/tte"
+   :xmlns/vendor "http://www.gnucash.org/XML/vendor"
+   })
+
 (defn document-element [{:keys [book counters]}]
-  (xml-element :gnc-v2 nil
+  (xml-element :gnc-v2 prefixes
     [(book-element book)]
     (map countdata-element counters)))
